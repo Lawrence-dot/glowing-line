f4b1ebd8110cd82e4d7ff1679e948b778aaf35bc
Lawrence-dot <69716037+Lawrence-dot@users.noreply.github.com>
2020-08-25 17:39:44 -0700

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -24,7 +24,7 @@ close.onclick = function () {
     page.style.paddingLeft = "0px";
 };
 
-
+var addButton = document.getElementById("add-btns");
 var date = document.getElementById("date");
 var table = document.getElementsByClassName("Data-table");
 var form = document.getElementsByClassName("main-form");
@@ -32,7 +32,7 @@ var dateT = document.getElementById("dateT");
 var time = setInterval(time, 1000);
 var displayDate = new Date();
 var serialN = 1;
-var id = 1;
+
 
 function time() {
     var display = new Date();
@@ -42,10 +42,34 @@ function time() {
     date.innerHTML = showDisplay;
 };
 
-var LIST = [];
+
+var LIST;
+var id;
+
+var savedList = localStorage.getItem("LIST");
+
+if (savedList) {
+    LIST = JSON.parse(savedList);
+     id = 1;
+    loadList(LIST);
+
+} else {
+    LIST = [];
+    id = 0;
+}
 
 
-function addToDo (){
+
+
+function loadList(array) {
+    array.forEach(element => {
+        addToDo(element.name, element.status, element.serialNumber);
+    })
+};
+
+
+
+function addToDo (inputted){
     let activity = document.getElementById("Activity-name");
     let table = document.getElementById("dataTable");
     let displayD = displayDate.toDateString();
@@ -56,70 +80,71 @@ function addToDo (){
     let statusP = document.getElementById("statusT");
     dateT.value = displayDT;
     let rows = document.getElementsByClassName("row");
-    var addBtn = document.getElementById("add-btns");3
+    var addBtn = document.getElementById("add-btns");
     let selectCol = document.getElementById("selectV");
     let selectedOption = selectCol.value;
 
-    if (activity.value) {
-        let newros = `<tr class="row nrow" id="row${id}">
-                            <td class="colserial" id="Text${id}"> </td>
+    let newros = `<tr class="row nrow" id="row${id}">
+                            <td class="colserial" id="Text${id}"> ${id} </td>
                             <td class="cols"> ${displayDT} </td>
-                            <td class="cols"> ${activity.value} </td>
+                            <td class="cols"> ${inputted} </td>
                             <td class="cols"> 
                              <p id= "statusT">  ${selectedOption}  </p>
-                            </td>
+                             </td>
                             <td class="cols"> <button onclick ="Editactivity()" id="edit-btn"> &#9998; </button> </td>
                             <td class="cols"> <button class="delete-btn" onclick = "Deletetodo()">  &#128465; </button> </td>
                             <td class="cols"> <button onclick="updateTodo()" class="update-btn"> Update </button> </td>
                           </tr>`;
         activity.value = "";
-        serialN++;
         id++;
     
         let neww = document.createElement("tr");
         neww.innerHTML = newros;
         table.appendChild(neww);
-        LIST.push(neww);
 
-
-         LIST.forEach( (col) =>{
-             let columns = col;
-             let colP = col.childNodes[1];
-             let colIndex = LIST.indexOf(columns);
-             let numbers = colIndex + 1;
-             colP.innerHTML = numbers;
-             console.log(numbers);
-         });
-    
-        
-    
-        // for(i=0; i<LIST.length; i++){
-        //     let selectCol = document.getElementById("select");
-        //     let selectedOption = selectCol.selectedOptions;
-        //     let statusText = document.getElementById("statusT");
-        //     statusText.innerText = "H";
-        //     console.log(selectedOption);
-        // };
-    }
  };
+
+addButton.addEventListener("click", function (addNew) {
+    let activity = document.getElementById("Activity-name");
+    let inputted = activity.value;
+   
+    // for (let i = 0; i < savedList.length; i++) {
+    //     const element = savedList[i];
+    //     console.log(element);
+    // };
+    
+    if (inputted) {
+        addToDo(inputted);
+       
+     
+        LIST.push({
+            name: inputted,
+            serialNumber: id,
+        });
+        id++;
+    }
+    localStorage.setItem("LIST", JSON.stringify(LIST));
+    location.reload();
+ });
+
+
 
 function Deletetodo(element) {
     let child = event.target;
     let rows = document.getElementsByClassName("row");
     let table = document.getElementById("dataTable");
     let serialC = document.getElementsByClassName("colserial");
-  
-    LIST.forEach( (col) =>{
+    table.removeChild(child.parentNode.parentNode);
+   
+    LIST.forEach(col => {
         LIST.splice(col, 1);
-        let columns = col;
-        let colP = col.childNodes[1];
-        let colIndex = LIST.indexOf(columns);
-        var numbers = colIndex + 1;
-        table.removeChild(col);
-        colP.innerHTML = numbers;
-        console.log(numbers);
+        console.log(col);
+        location.reload();
     });
+    localStorage.setItem("LIST", JSON.stringify(LIST));
 };
+
+// localStorage.setItem("List", JSON.stringify(LIST));
 
 function Editactivity(element) {
     let child = event.target;
@@ -154,21 +179,8 @@ function updateTodo (){
         let selectedOption = selectCol.value;
         let editStatus = col.childNodes[7];
         editStatus.innerText = selectedOption;
+        
     });
-
 };
 
-// document.addEventListener("keyup", function (addClick) {
-//     let activity = document.getElementById("Activity-name");
-  
-//     if(addClick.keycode == 13){
-//         if(activity.value){
-//             addToDo(enterKey);
-//         }
-//     }
-// });
-    
-
-
-
-
+//localStorage.clear();
